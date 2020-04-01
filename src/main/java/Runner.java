d49bdfa5a39ce1c84f33e4b246302d3e4d23ea02
ukalesny <u.kalesny@andersenlab.com>
2020-04-02 02:15:47 +0300

    Vanilla Java Code

---
+++ src/main/java/Runner.java
@@ -0,0 +1,8 @@
+import java14.Person;
+
+public class Runner {
+    public static void main(String[] args) {
+        final Person uladzimir = new Person("Uladzimir", 28);
+        System.out.println(uladzimir);
+    }
+}
---
+++ src/main/java/java14/Person.java
@@ -0,0 +1,43 @@
+package java14;
+
+import java.util.Objects;
+
+public class Person {
+    private final String name;
+    private final int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || this.getClass() != o.getClass()) return false;
+        final Person person = (Person) o;
+        return this.age == person.age &&
+                this.name.equals(person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.name, this.age);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
