5ae80492c47c2797a6b0d6c29f8b1c5bf66c201a
ukalesny <u.kalesny@andersenlab.com>
2020-04-02 03:58:48 +0300

    Records in action

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,8 +1,12 @@
 import java14.Person;
 
+import java.util.Arrays;
+
 public class Runner {
     public static void main(String[] args) {
         final Person uladzimir = new Person("Uladzimir", 28);
+        System.out.println(uladzimir.getClass().isRecord()); // true
+        System.out.println(Arrays.toString(uladzimir.getClass().getRecordComponents())); // [java.lang.String name, int age]
         System.out.println(uladzimir);
     }
 }
--- src/main/java/java14/Person.java
+++ src/main/java/java14/Person.java
@@ -1,43 +1,48 @@
 package java14;
 
-import java.util.Objects;
+/**
+ * public final class Person extends java.lang.Record {
+ *      private final java.lang.String name;
+ *      private final int age;
+ *      public Person(java.lang.String name, int age)
+ *      public java.lang.String toString()
+ *      public final int hashCode()
+ *      public final boolean equals(java.lang.Object o)
+ *      public java.lang.String name()
+ *      public int age()
+ * }
+ * We can implements interfaces
+ * We can define any other methods
+ * We can declare static constants
+ * We can override any methods (hashcode, equals, toString) and so on
+ * But we cannot to use extends
+ */
+public record Person(String name, int age) {
+    private static final int DEFAULT_AGE = 28;
 
-public class Person {
-    private final String name;
-    private final int age;
+    /**
+     * Canonical constructor
+     * @param name
+     * @param age
+     */
+//    public Person(final String name, final int age) {
+//        this.name = name;
+//        this.age = age;
+//    }
 
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
+
+    public Person(final String name) {
+        this(name, DEFAULT_AGE);
     }
 
-    public String getName() {
-        return this.name;
-    }
-
-    public int getAge() {
-        return this.age;
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) return true;
-        if (o == null || this.getClass() != o.getClass()) return false;
-        final Person person = (Person) o;
-        return this.age == person.age &&
-                this.name.equals(person.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(this.name, this.age);
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
+    /**
+     * Compact constructor
+     * @param name
+     * @param age
+     */
+    public Person {
+        if (age < 0) {
+            throw new IllegalArgumentException("Invalid age " + age);
+        }
     }
 }
