abd90d3f5d4b100f9dacf0912f8351023e0dfd6a
ukalesny <u.kalesny@andersenlab.com>
2020-04-02 04:13:42 +0300

    NPE description in action

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -8,5 +8,15 @@ public class Runner {
         System.out.println(uladzimir.getClass().isRecord()); // true
         System.out.println(Arrays.toString(uladzimir.getClass().getRecordComponents())); // [java.lang.String name, int age]
         System.out.println(uladzimir);
+
+        final Person nullablePerson = new Person(null, 24);
+        System.out.println(nullablePerson.name().isBlank()); // nullablePerson.name() -> NPE
+        // If you want to defend yourself with NPE you can add into
+        // "Edit Configuration|VM Options" key -> -XX:+ShowCodeDetailsInExceptionMessages
+        // Cannot invoke "String.isBlank()" because the return value of "java14.Person.name()" is null
+        // Why we dont use that features by default in the production profile?
+        // Security(message with internal code)!
+        // Performance (collection Info)!
+        // Compatibility
     }
 }
